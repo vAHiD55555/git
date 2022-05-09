caad709282603ff05159d441698918767e8fa2d9
Glen Choo <chooglen@google.com>
2022-05-09 10:50:31 -0700

    pull: only pass '--recurse-submodules' to subcommands
    Fix a bug in "git pull" where `submodule.recurse` is preferred over
    `fetch.recurseSubmodules` (Documentation/config/fetch.txt says that
    `fetch.recurseSubmodules` should be preferred.). Do this by passing the
    value of the "--recurse-submodules" CLI option to the underlying fetch,
    instead of passing a value that combines the CLI option and config
    variables.
    
    In other words, this bug occurred because builtin/pull.c is conflating
    two similar-sounding, but different concepts:
    
    - Whether "git pull" itself should care about submodules e.g. whether it
      should update the submodule worktrees after performing a merge.
    - The value of "--recurse-submodules" to pass to the underlying "git
      fetch".
    
    Thus, when `submodule.recurse` is set, the underlying "git fetch" gets
    invoked with "--recurse-submodules", overriding the value of
    `fetch.recurseSubmodules`.
    
    An alternative (and more obvious) approach to fix the bug would be to
    teach "git pull" to understand `fetch.recurseSubmodules`, but the
    proposed solution works better because:
    
    - We don't maintain two identical config-parsing implementions in "git
      pull" and "git fetch".
    - It works better with other commands invoked by "git pull" e.g. "git
      merge" won't accidentally respect `fetch.recurseSubmodules`.
    
    Reported-by: Huang Zou <huang.zou@schrodinger.com>
    Helped-by: Philippe Blain <levraiphilippeblain@gmail.com>
    Signed-off-by: Glen Choo <chooglen@google.com>

--- t/t5572-pull-submodule.sh
+++ t/t5572-pull-submodule.sh
@@ -107,6 +107,20 @@ test_expect_success " --[no-]recurse-submodule and submodule.recurse" '
 	test_path_is_file super/sub/merge_strategy_4.t
 '
 
+test_expect_success "fetch.recurseSubmodules option triggers recursive fetch (but not recursive update)" '
+	test_commit -C child merge_strategy_5 &&
+	git -C parent submodule update --remote &&
+	git -C parent add sub &&
+	git -C parent commit -m "update submodule" &&
+
+	git -C super -c fetch.recursesubmodules=true pull --no-rebase &&
+	# Check that the submodule commit was fetched
+	sub_oid=$(git -C super rev-parse FETCH_HEAD:sub) &&
+	git -C super/sub cat-file -e $sub_oid &&
+	# Check that the submodule worktree did not update
+	! test_path_is_file super/sub/merge_strategy_5.t
+'
+
 test_expect_success 'pull --rebase --recurse-submodules (remote superproject submodule changes, local submodule changes)' '
 	# This tests the following scenario :
 	# - local submodule has new commits
