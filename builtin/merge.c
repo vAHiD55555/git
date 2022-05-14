88bdca72a780d70e156e22e1ab96dedd368c761b
Elijah Newren <newren@gmail.com>
2022-05-14 11:00:37 -0700

    merge: make restore_state() do as its name says
    Previously, if the user:
    
    * Had no local changes before starting the merge
    * A merge strategy makes changes to the working tree/index but returns
      with exit status 2
    
    Then we'd call restore_state() to clean up the changes and either let
    the next merge strategy run (if there is one), or exit telling the user
    that no merge strategy could handle the merge.  Unfortunately,
    restore_state() did not clean up the changes as expected; that function
    was a no-op if the stash was a null, and the stash would be null if
    there were no local changes before starting the merge.  So, instead of
    "Rewinding the tree to pristine..." as the code claimed, restore_state()
    would leave garbage around in the index and working tree (possibly
    including conflicts) for either the next merge strategy or for the user
    after aborting the merge.  And in the case of aborting the merge, the
    user would be unable to run "git merge --abort" to get rid of the
    unintended leftover conflicts, because the merge control files were not
    written as it was presumed that we had restored to a clean state
    already.
    
    Fix the main problem by making sure that restore_state() only skips the
    stash application if the stash is null rather than skipping the whole
    function.
    
    However, there is a secondary problem -- since merge.c forks
    subprocesses to do the cleanup, the in-memory index is left out-of-sync.
    While there was a refresh_cache(REFRESH_QUIET) call that attempted to
    correct that, that function would not handle cases where the previous
    merge strategy added conflicted entries.  We need to drop the index and
    re-read it to handle such cases.
    
    (Alternatively, we could stop forking subprocesses and instead call some
    appropriate function to do the work which would update the in-memory
    index automatically.  For now, just do the simple fix.)
    
    Reported-by: ZheNing Hu <adlternative@gmail.com>
    Signed-off-by: Elijah Newren <newren@gmail.com>

--- builtin/merge.c
+++ builtin/merge.c
@@ -377,10 +377,10 @@ static void restore_state(const struct object_id *head,
 {
 	const char *args[] = { "stash", "apply", NULL, NULL };
 
-	if (is_null_oid(stash))
-		return;
-
 	reset_hard(head, 1);
+
+	if (is_null_oid(stash))
+		goto refresh_cache;
 
 	args[2] = oid_to_hex(stash);
 
@@ -390,7 +390,9 @@ static void restore_state(const struct object_id *head,
 	 */
 	run_command_v_opt(args, RUN_GIT_CMD);
 
-	refresh_cache(REFRESH_QUIET);
+refresh_cache:
+	if (discard_cache() < 0 || read_cache() < 0)
+		die(_("could not read index"));
 }
 
 /* This is called when no merge was necessary. */
