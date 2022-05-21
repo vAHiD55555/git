de794d0af185fae84bc31f2d54cd0e3cd867e4d6
Carl Smedstad <carl.smedstad@protonmail.com>
2022-05-21 17:22:12 +0200

    check-ignore: --non-matching without --verbose
    Allow --non-matching to be specified without the --verbose flag to make
    git only list files that are not matching any pattern. The behaviour
    when specifying both --non-matching and --verbose is unchanged.
    
    The motivation for this is to use the underutilized invocation of
    --non-matching without --verbose to create a shorthand for what could be
    achieved by:
    
        find . |
          git check-ignore --verbose --non-matching |
          grep $'^::\t' |
          sed -e 's/.*\t//'
    
    Signed-off-by: Carl Smedstad <carl.smedstad@protonmail.com>

--- builtin/check-ignore.c
+++ builtin/check-ignore.c
@@ -113,8 +113,17 @@ static int check_ignore(struct dir_struct *dir,
 			    pattern->flags & PATTERN_FLAG_NEGATIVE)
 				pattern = NULL;
 		}
-		if (!quiet && (pattern || show_non_matching))
-			output_pattern(pathspec.items[i].original, pattern);
+		if (!quiet) {
+			if (verbose) {
+				if (show_non_matching || pattern)
+					output_pattern(pathspec.items[i].original, pattern);
+			} else {
+				if (show_non_matching && !pattern)
+					output_pattern(pathspec.items[i].original, pattern);
+				if (!show_non_matching && pattern)
+					output_pattern(pathspec.items[i].original, pattern);
+			}
+		}
 		if (pattern)
 			num_ignored++;
 	}
@@ -175,8 +184,6 @@ int cmd_check_ignore(int argc, const char **argv, const char *prefix)
 		if (verbose)
 			die(_("cannot have both --quiet and --verbose"));
 	}
-	if (show_non_matching && !verbose)
-		die(_("--non-matching is only valid with --verbose"));
 
 	/* read_cache() is only necessary so we can watch out for submodules. */
 	if (!no_index && read_cache() < 0)
