265342d59db93fd01df303ba8b4c4574e0a0dfde
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2022-05-25 12:03:57 +0200

    CI: combine ci/install{,-docker}-dependencies.sh
    Combine the "ci/install-dependencies.sh" and
    "ci/install-docker-dependencies.sh" into one script. The "case"
    statement in the latter only cared about "$jobname", and can be folded
    into the same "case" statement in the former.
    
    The reason they split up is historical, until a preceding commit
    "ci/lib.sh" required "bash", which might not have been available in
    "docker".
    
    This also fixes issue in "ci/install-docker-dependencies.sh" where
    we'd hide errors due to not using "set -e". Now that we include
    "ci/lib.sh" we won't have that potential issue.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- ci/install-dependencies.sh
+++ ci/install-dependencies.sh
@@ -99,6 +99,21 @@ linux-gcc-default)
 	sudo apt-get -q update
 	sudo apt-get -q -y install $UBUNTU_COMMON_PKGS
 	;;
+linux32)
+	linux32 --32bit i386 sh -c '
+		apt update >/dev/null &&
+		apt install -y build-essential libcurl4-openssl-dev \
+			libssl-dev libexpat-dev gettext python >/dev/null
+	'
+	;;
+linux-musl)
+	apk add --update build-base curl-dev openssl-dev expat-dev gettext \
+		pcre2-dev python3 musl-libintl perl-utils ncurses >/dev/null
+	;;
+pedantic)
+	dnf -yq update >/dev/null &&
+	dnf -yq install make gcc findutils diffutils perl python3 gettext zlib-devel expat-devel openssl-devel curl-devel pcre2-devel >/dev/null
+	;;
 esac
 
 if type p4d >/dev/null 2>&1 && type p4 >/dev/null 2>&1
