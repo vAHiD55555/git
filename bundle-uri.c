6a1504b1c3a24b45d48c093285dfcc9a3d6afd68
Derrick Stolee <derrickstolee@github.com>
2022-12-16 13:00:09 -0500

    bundle-uri: store fetch.bundleCreationToken
    When a bundle list specifies the "creationToken" heuristic, the Git
    client downloads the list and then starts downloading bundles in
    descending creationToken order. This process stops as soon as all
    downloaded bundles can be applied to the repository (because all
    required commits are present in the repository or in the downloaded
    bundles).
    
    When checking the same bundle list twice, this strategy requires
    downloading the bundle with the maximum creationToken again, which is
    wasteful. The creationToken heuristic promises that the client will not
    have a use for that bundle if its creationToken value is at most the
    previous creationToken value.
    
    To prevent these wasteful downloads, create a fetch.bundleCreationToken
    config setting that the Git client sets after downloading bundles. This
    value allows skipping that maximum bundle download when this config
    value is the same value (or larger).
    
    To test that this works correctly, we can insert some "duplicate"
    fetches into existing tests and demonstrate that only the bundle list is
    downloaded.
    
    The previous logic for downloading bundles by creationToken worked even
    if the bundle list was empty, but now we have logic that depends on the
    first entry of the list. Terminate early in the (non-sensical) case of
    an empty bundle list.
    
    Signed-off-by: Derrick Stolee <derrickstolee@github.com>

--- bundle-uri.c
+++ bundle-uri.c
@@ -487,6 +487,8 @@ static int fetch_bundles_by_token(struct repository *r,
 {
 	int cur;
 	int move_direction = 0;
+	const char *creationTokenStr;
+	uint64_t maxCreationToken = 0, newMaxCreationToken = 0;
 	struct bundle_list_context ctx = {
 		.r = r,
 		.list = list,
@@ -500,7 +502,26 @@ static int fetch_bundles_by_token(struct repository *r,
 
 	for_all_bundles_in_list(list, append_bundle, &bundles);
 
+	if (!bundles.nr) {
+		free(bundles.items);
+		return 0;
+	}
+
 	QSORT(bundles.items, bundles.nr, compare_creation_token_decreasing);
+
+	/*
+	 * If fetch.bundleCreationToken exists, parses to a uint64t, and
+	 * is not strictly smaller than the maximum creation token in the
+	 * bundle list, then do not download any bundles.
+	 */
+	if (!repo_config_get_value(r,
+				   "fetch.bundlecreationtoken",
+				   &creationTokenStr) &&
+	    sscanf(creationTokenStr, "%"PRIu64, &maxCreationToken) == 1 &&
+	    bundles.items[0]->creationToken <= maxCreationToken) {
+		free(bundles.items);
+		return 0;
+	}
 
 	/*
 	 * Attempt to download and unbundle the minimum number of bundles by
@@ -522,6 +543,16 @@ static int fetch_bundles_by_token(struct repository *r,
 	cur = 0;
 	while (cur >= 0 && cur < bundles.nr) {
 		struct remote_bundle_info *bundle = bundles.items[cur];
+
+		/*
+		 * If we need to dig into bundles below the previous
+		 * creation token value, then likely we are in an erroneous
+		 * state due to missing or invalid bundles. Halt the process
+		 * instead of continuing to download extra data.
+		 */
+		if (bundle->creationToken <= maxCreationToken)
+			break;
+
 		if (!bundle->file) {
 			/*
 			 * Not downloaded yet. Try downloading.
@@ -561,6 +592,9 @@ static int fetch_bundles_by_token(struct repository *r,
 				 */
 				move_direction = -1;
 				bundle->unbundled = 1;
+
+				if (bundle->creationToken > newMaxCreationToken)
+					newMaxCreationToken = bundle->creationToken;
 			}
 		}
 
@@ -575,14 +609,24 @@ stack_operation:
 		cur += move_direction;
 	}
 
-	free(bundles.items);
-
 	/*
 	 * We succeed if the loop terminates because 'cur' drops below
 	 * zero. The other case is that we terminate because 'cur'
 	 * reaches the end of the list, so we have a failure no matter
 	 * which bundles we apply from the list.
 	 */
+	if (cur < 0) {
+		struct strbuf value = STRBUF_INIT;
+		strbuf_addf(&value, "%"PRIu64"", newMaxCreationToken);
+		if (repo_config_set_multivar_gently(ctx.r,
+						    "fetch.bundleCreationToken",
+						    value.buf, NULL, 0))
+			warning(_("failed to store maximum creation token"));
+
+		strbuf_release(&value);
+	}
+
+	free(bundles.items);
 	return cur >= 0;
 }
 
