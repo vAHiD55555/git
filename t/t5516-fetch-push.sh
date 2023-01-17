966cb49c388b652861c773ad7430875bf7896c16
ZheNing Hu <adlternative@gmail.com>
2023-01-17 15:35:49 +0800

    [RFC] push: allow delete one level ref
    Git will reject the deletion of one level refs e,g. "refs/foo"
    through "git push -d", however, some users want to be able to
    clean up these branches that were created unexpectedly on the
    remote.
    
    Therefore, when updating branches on the server with
    "git receive-pack", by checking whether it is a branch deletion
    operation, it will determine whether to allow the update of
    one level refs. This avoids creating/updating such one level
    branches, but allows them to be deleted.
    
    On the client side, "git push" also does not properly fill in
    the old-oid of one level refs, which causes the server-side
    "git receive-pack" to think that the ref's old-oid has changed
    when deleting one level refs, this causes the push to be rejected.
    
    So the solution is to fix the client to be able to delete
    one level refs by properly filling old-oid.
    
    Signed-off-by: ZheNing Hu <adlternative@gmail.com>

--- t/t5516-fetch-push.sh
+++ t/t5516-fetch-push.sh
@@ -903,6 +903,13 @@ test_expect_success 'push --delete refuses empty string' '
 	test_must_fail git push testrepo --delete ""
 '
 
+test_expect_success 'push --delete onelevel refspecs' '
+	mk_test testrepo heads/main &&
+	git -C testrepo update-ref refs/onelevel refs/heads/main &&
+	git push testrepo --delete refs/onelevel &&
+	test_must_fail git -C testrepo rev-parse --verify refs/onelevel
+'
+
 test_expect_success 'warn on push to HEAD of non-bare repository' '
 	mk_test testrepo heads/main &&
 	(
