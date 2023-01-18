8fe920c653ed1c025a759a45ae752fcd5ee7771d
Seija Kijin <doremylover123@gmail.com>
2023-01-18 17:34:15 -0500

    mingw: replace deprecated GetVersion with RtlGetVersion
    The previous way is deprecated and returns
    the wrong value in Windows 8 and up,
    returning the manifest Windows data
    as opposed to the actual Windows data.
    
    RtlGetVersion is the correct way to get the Windows version now.
    
    Signed-off-by: Seija Kijin <doremylover123@gmail.com>

--- compat/mingw.c
+++ compat/mingw.c
@@ -3081,15 +3081,36 @@ int wmain(int argc, const wchar_t **wargv)
 	return exit_status;
 }
 
+/*
+ * For RtlGetVersion in uname
+ */
+
+typedef NTSTATUS(WINAPI *RtlGetVersionPtr)(PRTL_OSVERSIONINFOW);
+union winprocaddr {
+	FARPROC procaddr;
+	RtlGetVersionPtr procGetVersion;
+};
+
 int uname(struct utsname *buf)
 {
-	unsigned v = (unsigned)GetVersion();
+	union winprocaddr RtlGetVersionInternal;
+	OSVERSIONINFOA version;
+
+	RtlGetVersionInternal.procaddr =
+		GetProcAddress(GetModuleHandleW(L"ntdll.dll"), "RtlGetVersion");
+	if (!RtlGetVersionInternal.procaddr) {
+		die_message_errno(
+			"Could not get handle to RtlGetVersion in ntdll.dll");
+	}
+
+	version.dwOSVersionInfoSize = sizeof(OSVERSIONINFO);
+	RtlGetVersionInternal.procGetVersion((PRTL_OSVERSIONINFOW)&version);
+
 	memset(buf, 0, sizeof(*buf));
 	xsnprintf(buf->sysname, sizeof(buf->sysname), "Windows");
-	xsnprintf(buf->release, sizeof(buf->release),
-		 "%u.%u", v & 0xff, (v >> 8) & 0xff);
-	/* assuming NT variants only.. */
-	xsnprintf(buf->version, sizeof(buf->version),
-		  "%u", (v >> 16) & 0x7fff);
+	xsnprintf(buf->release, sizeof(buf->release), "%lu.%lu",
+		  version.dwMajorVersion, version.dwMinorVersion);
+	xsnprintf(buf->version, sizeof(buf->version), "%lu",
+		  version.dwBuildNumber);
 	return 0;
 }
