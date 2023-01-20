a8b5a0e4fb01bb3f4332b94f7e82e10d946b7828
Elijah Newren <newren@gmail.com>
2023-01-20 11:03:52 -0800

    rebase: flag --apply and --merge as incompatible
    Previously, we flagged options which implied --apply as being
    incompatible with options which implied --merge.  But if both options
    were given explicitly, then we didn't flag the incompatibility.  The
    same is true with --apply and --interactive.  Add the check, and add
    some testcases to verify these are also caught.
    
    Signed-off-by: Elijah Newren <newren@gmail.com>

--- builtin/rebase.c
+++ builtin/rebase.c
@@ -920,6 +920,9 @@ static int parse_opt_am(const struct option *opt, const char *arg, int unset)
 	BUG_ON_OPT_NEG(unset);
 	BUG_ON_OPT_ARG(arg);
 
+	if (opts->type != REBASE_UNSPECIFIED && opts->type != REBASE_APPLY)
+	    die(_("apply options and merge options cannot be used together"));
+
 	opts->type = REBASE_APPLY;
 
 	return 0;
@@ -933,8 +936,10 @@ static int parse_opt_merge(const struct option *opt, const char *arg, int unset)
 	BUG_ON_OPT_NEG(unset);
 	BUG_ON_OPT_ARG(arg);
 
-	if (!is_merge(opts))
-		opts->type = REBASE_MERGE;
+	if (opts->type != REBASE_UNSPECIFIED && opts->type != REBASE_MERGE)
+	    die(_("apply options and merge options cannot be used together"));
+
+	opts->type = REBASE_MERGE;
 
 	return 0;
 }
@@ -947,6 +952,9 @@ static int parse_opt_interactive(const struct option *opt, const char *arg,
 
 	BUG_ON_OPT_NEG(unset);
 	BUG_ON_OPT_ARG(arg);
+
+	if (opts->type != REBASE_UNSPECIFIED && opts->type != REBASE_MERGE)
+	    die(_("apply options and merge options cannot be used together"));
 
 	opts->type = REBASE_MERGE;
 	opts->flags |= REBASE_INTERACTIVE_EXPLICIT;
