44a4027b240289605f4c40b9121ee6615e9759f7
Elijah Newren <newren@gmail.com>
2023-04-27 11:03:02 -0700

    hash-ll, hashmap: move oidhash() to hash-ll
    oidhash() was used by both hashmap and khash, which makes sense.
    However, the location of this function in hashmap.[ch] meant that
    khash.h had to depend upon hashmap.h, making people unfamiliar with
    khash think that it was built upon hashmap.  (Or at least, I personally
    was confused for a while about this in the past.)
    
    Move this function to hash-ll, so that khash.h can stop depending upon
    hashmap.h.
    
    This has another benefit as well: it allows us to remove hashmap.h's
    dependency on hash-ll.h.  While some callers of hashmap.h were making
    use of oidhash, most were not, so this change provides another way to
    reduce the number of includes.
    
    Diff best viewed with `--color-moved`.  Note that there's a small but
    important change to khash.h in this patch as well to allow this move,
    which is easy to overlook.
    
    Signed-off-by: Elijah Newren <newren@gmail.com>

--- merge-blobs.c
+++ merge-blobs.c
@@ -4,7 +4,7 @@
 #include "ll-merge.h"
 #include "blob.h"
 #include "merge-blobs.h"
-#include "object-store.h"
+#include "object-store-ll.h"
 
 static int fill_mmfile_blob(mmfile_t *f, struct blob *obj)
 {
